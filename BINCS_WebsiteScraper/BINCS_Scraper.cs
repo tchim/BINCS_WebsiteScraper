a7c3872eca65cef0adc170ae29087653c45ba252
tchim <terry.chim@gmail.com>
2013-08-02 02:23:01 -0400

    First publish
    Testing how to use GIT

---
+++ BINCS_WebsiteScraper/BINCS_Scraper.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+/*******************************************************************************
+Web Scraper for Defense Logicstics Information Service CAGE Codes
+ *******************************************************************************/
+
+namespace DLA_Scraper
+{
+    public class DLA
+    {
+        public string Company_Name { get; set; }
+        public string Address { get; set; }
+        public string CAO_ADP { get; set; }
+
+
+
+
+        public bool status { get; set; }
+
+        //Search Engine URLs
+        private string DLASearch = "https://www.logisticsinformationservice.dla.mil/BINCS/details.aspx?CAGE=";
+
+        //Constructor
+        public DLA(string p_CageCode)
+        {
+            new DLA(p_CageCode, true);
+        }
+
+        //Default parameter specifiers are not permitted
+        public DLA(string p_CageCode, bool GetExtraInfo)
+        //public BINCS(string MovieName, bool GetExtraInfo = true)
+        {
+            string BINCSUrl = generateUrl(System.Uri.EscapeUriString(p_CageCode));
+            status = false;
+            if (!string.IsNullOrEmpty(BINCSUrl))
+            {
+                parseResultPage(BINCSUrl, GetExtraInfo);
+            }
+        }
+
+        //Get BINCS URL from search results
+        private string generateUrl(string p_CageCode)
+        {
+            string searchEngine = "DLASearch";
+            return generateUrl(p_CageCode, searchEngine);
+        }
+
+        private string generateUrl(string p_CageCode, string p_searchEngine)
+        //private string getBINCSUrl(string MovieName, string searchEngine = "google")
+        {
+            string url = DLASearch + p_CageCode;
+
+            return url;
+        }
+
+        //Parse BINCS page data
+        private void parseResultPage(string p_url, bool GetExtraInfo)
+        {
+            string temp;
+            Match m1;
+
+            string html = getUrlData(p_url);
+            //html = stripCode(html);
+
+            string s_no_matching_result_pattern = "<form name=\"aspnetForm\" method=\"post\" action=\"begin_search.aspx\" id=\"aspnetForm\">";
+            //string s_noResult
+            m1 = Regex.Match(html, s_no_matching_result_pattern);
+
+            if (m1.Success)
+            {
+                throw new ArgumentException("No result found for this CAGE code OR BINCs website has changed output.");
+            }
+
+            string s_company_pre_pattern = "Company Name:</label>\\s*</td>\\s*<td>\\s*<span id\\s*=\\s*[\"'].*cphMainPageBody_lblCompNameData[\"']>";
+            string s_company_post_pattern = "</span>";
+            string s_company_pattern = s_company_pre_pattern + "(?<1>.*)" + s_company_post_pattern;
+            m1 = Regex.Match(html, s_company_pattern);
+            temp = Regex.Replace(m1.Value, s_company_pre_pattern, string.Empty);
+            temp = temp.Remove(temp.IndexOf(s_company_post_pattern));
+            Company_Name = temp;
+
+
+            string s_address_pre_pattern = "Address:</label>\\s*</td>\\s*<td>\\s*<span id\\s*=\\s*[\"'].*_cphMainPageBody_lblAddressData[\"']>";
+            string s_address_post_pattern = "</span>";
+            string s_address_pattern = s_address_pre_pattern + "(?<1>.*)" + s_address_post_pattern;
+            m1 = Regex.Match(html, s_address_pattern);
+            temp = Regex.Replace(m1.Value, s_address_pre_pattern, string.Empty);
+            temp = temp.Remove(temp.IndexOf(s_address_post_pattern));
+            Address = temp; ;
+
+
+            string s_CAO_ADP_pre_pattern = "CAO-ADP:</label>\\s*</td>\\s*<td>\\s*<span id\\s*=\\s*[\"'].*_cphMainPageBody_lblCaoAdpData[\"']>";
+            string s_CAO_ADP_post_pattern = "</span>";
+            string s_CAO_ADP_pattern = s_CAO_ADP_pre_pattern + "(?<1>.*)" + s_CAO_ADP_post_pattern;
+            m1 = Regex.Match(html, s_CAO_ADP_pattern);
+            temp = Regex.Replace(m1.Value, s_CAO_ADP_pre_pattern, string.Empty);
+            temp = temp.Remove(temp.IndexOf(s_CAO_ADP_post_pattern));
+            CAO_ADP = temp;
+
+        }
+
+
+        public string stripCode(string the_html)
+        {
+            // Remove google analytics code and other JS
+            the_html = Regex.Replace(the_html, "<script.*?</script>", "", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            // Remove inline stylesheets
+            the_html = Regex.Replace(the_html, "<style.*?</style>", "", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            // Remove HTML tags
+            the_html = Regex.Replace(the_html, "</?[a-z][a-z0-9]*[^<>]*>", "");
+            // Remove HTML comments
+            the_html = Regex.Replace(the_html, "<!--(.|\\s)*?-->", "");
+            // Remove Doctype
+            the_html = Regex.Replace(the_html, "<!(.|\\s)*?>", "");
+            // Remove excessive whitespace
+            the_html = Regex.Replace(the_html, "[\t\r\n]", " ");
+
+            return the_html;
+        }
+
+        /*******************************[ Helper Methods ]********************************/
+
+        //Match single instance
+        private string match(string regex, string html)
+        {
+            int i = 1;
+            return match(regex, html, i);
+        }
+
+        private string match(string regex, string html, int i)
+        //private string match(string regex, string html, int i = 1)
+        {
+            return new Regex(regex, RegexOptions.Multiline).Match(html).Groups[i].Value.Trim();
+        }
+
+        //Match all instances and return as ArrayList
+        private ArrayList matchAll(string regex, string html)
+        {
+            int i = 1;
+            return matchAll(regex, html, i);
+        }
+
+        private ArrayList matchAll(string regex, string html, int i)
+        //private ArrayList matchAll(string regex, string html, int i = 1)
+        {
+            ArrayList list = new ArrayList();
+            foreach (Match m in new Regex(regex, RegexOptions.Multiline).Matches(html))
+                list.Add(m.Groups[i].Value.Trim());
+            return list;
+        }
+
+        //Strip HTML Tags
+        static string StripHTML(string inputString)
+        {
+            return Regex.Replace(inputString, @"<.*?>", string.Empty);
+        }
+
+        //Get URL Data
+        private string getUrlData(string url)
+        {
+            WebClient client = new WebClient();
+            Random r = new Random();
+            //Random IP Address
+            client.Headers["X-Forwarded-For"] = r.Next(0, 255) + "." + r.Next(0, 255) + "." + r.Next(0, 255) + "." + r.Next(0, 255);
+            //Random User-Agent
+            client.Headers["User-Agent"] = "Mozilla/" + r.Next(3, 5) + ".0 (Windows NT " + r.Next(3, 5) + "." + r.Next(0, 2) + "; rv:2.0.1) Gecko/20100101 Firefox/" + r.Next(3, 5) + "." + r.Next(0, 5) + "." + r.Next(0, 5);
+
+
+            //Problem to solve: The underlying connection was closed: Could not establish trust relationship for the SSL/TLS secure channel.
+            //To by-pass create a new ServerCertificateValidationCallback 
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+
+            Stream datastream = client.OpenRead(url);
+            StreamReader reader = new StreamReader(datastream);
+            StringBuilder sb = new StringBuilder();
+            while (!reader.EndOfStream)
+                sb.Append(reader.ReadLine());
+            return sb.ToString();
+        }
+    }
+}
---
+++ BINCS_WebsiteScraper/Window1.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Threading;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace WpfApplication_WebSiteScraper21
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public delegate void UpdateTextCallback(string message);
+
+
+        private void TimerThread()
+        {
+            DateTime dt_start = DateTime.Now;
+            int n_frequency_times_per_second = 24;
+            int n_sleep_interval_period = Convert.ToInt32(Math.Round(Convert.ToDecimal(1000 / n_frequency_times_per_second)));
+            int MAX_INTEVALS = 100000000;
+            for (int i = 0; i <= MAX_INTEVALS; i++)
+            //while (i<= MAX_INTEVALS)
+            {
+                Thread.Sleep(n_sleep_interval_period);
+
+                //Calculate ELASPED seconds
+                Decimal n_elapsed_seconds = Convert.ToDecimal(DateTime.Now.Ticks - dt_start.Ticks) / 10000000;
+
+                string s_message = string.Format("Elapsed Time: {0:N2}", n_elapsed_seconds);
+                textBlock1.Dispatcher.Invoke(
+                    new UpdateTextCallback(this.UpdateText),
+                    new object[] { s_message }
+
+
+                );
+            }
+        }
+
+
+        private void TestThread(string p_input)
+        {
+
+            Thread my_TimerThread = new Thread(new ThreadStart(TimerThread));
+
+            my_TimerThread.Start();
+            string s_results = string.Empty;
+            try
+            {
+                s_results = BINCS_Lookup(p_input);
+            }
+            catch (Exception ex)
+            {
+                s_results = ex.Message;
+            }
+            finally
+            {
+                textBlock1.Dispatcher.Invoke(
+                    new UpdateTextCallback(this.UpdateTextAndEnableButton),
+                   new object[] { s_results }
+
+                );
+
+                my_TimerThread.Abort();
+            }
+        }
+
+        private void UpdateText(string message)
+        {
+            textBlock1.Text = message;
+        }
+
+        private void UpdateTextAndEnableButton(string message)
+        {
+            textBlock1.Text = message;
+            button1.IsEnabled = true;
+            button1.Content = "Lookup Defense Logistics Info";
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            string s_movie_name = textBox_Title_Search.Text.Trim();
+            if (!String.IsNullOrEmpty(s_movie_name))
+            {
+                button1.Content = "Searching...";
+                button1.IsEnabled = false;
+                Thread test = new Thread(() => TestThread(s_movie_name));
+                test.Start();
+            }
+            else
+            {
+                MessageBox.Show("Please input a title to search");
+            }
+        }
+
+
+
+
+        private string IMDB_Lookup(string s_movie_name)
+        {
+            IMDb_Scraper.IMDb my_scraper = new IMDb_Scraper.IMDb(s_movie_name, false);
+
+
+            StringBuilder sb_output = new StringBuilder();
+
+            sb_output.Append(my_scraper.Title);
+            sb_output.Append("\n");
+            sb_output.Append(my_scraper.Year);
+            return sb_output.ToString();
+        }
+
+        private string BINCS_Lookup(string s_cage_code)
+        {
+            DLA_Scraper.DLA my_scraper = new DLA_Scraper.DLA(s_cage_code, false);
+
+            StringBuilder sb_output = new StringBuilder();
+
+            sb_output.Append(my_scraper.Company_Name);
+            sb_output.Append("\n");
+            sb_output.Append(my_scraper.Address);
+            sb_output.Append("\n");
+            sb_output.Append(my_scraper.CAO_ADP);
+
+            return sb_output.ToString().Replace("<br />", "\n");
+        }
+    }
+}
